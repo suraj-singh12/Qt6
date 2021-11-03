80f1d30c94bb301930247357e8a882a0cb38e933
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-03 11:53:57 +0530

    retrieving value directly from widget in handler

--- app.py
+++ app.py
@@ -13,24 +13,43 @@ from PyQt5.QtCore import QSize
 
 # In Qt sizes are defined using a "QSize" object. This accepts width and height parameters in that order.
 
+# -------------------------------------------------------------------------------------------------------
+# Signals: Signals are notifications emitted by widgets when something happens.
+# That something can be any number of things, from pressing a button,
+# to the text of an input box changing, to the text of the window changing.
+# Many signals are initiated by user action, but this is not a rule.
+# In addition to notifying about something happening,
+# signals can also send data to provide additional context about what happened.
+
+# Slots: Slots is the name Qt uses for the receivers of signals.
+# In Python any function (or method) in your application can be used as a slot
+# -- simply by connecting the signal to it. If the signal sends data,
+# then the receiving function will receive that data too.
+# Many Qt widgets also have their own built-in slots, meaning you can hook Qt widgets together directly.
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My app")
-        button = QPushButton("Press Me!")
+        self.button = QPushButton("Press Me!")
         self.button_is_checked = True
 
-        button.setCheckable(True)
-        button.clicked.connect(self.button_was_clicked)
-        button.clicked.connect(self.button_was_toggled)
+        self.button.setCheckable(True)
+        self.button.released.connect(self.button_was_released)
+        # self.button.setChecked(self.button_is_checked)
+        # button.clicked.connect(self.button_was_toggled)
 
         self.setMinimumSize(QSize(400, 150))
         self.setMaximumSize(QSize(600, 400))
         # setting the window as fixed size, this will disable resizing capability
         # self.setFixedSize(QSize(400,400))
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
+
+    def button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print(self.button_is_checked)
 
     def button_was_clicked(self):
         print("clicked !")
