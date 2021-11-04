863a84f70fd4b83d0464295bb1b31b8a3480203d
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-04 10:01:25 +0530

    nesting QVBoxLayout and QHBoxLayout in a QVBoxLayout

---
+++ Layout Management/NestingLayouts/nesting1.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPalette, QColor
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setMinimumSize(QSize(400, 300))
+        self.setWindowTitle("My App")
+
+        layout1 = QVBoxLayout()
+        layout2 = QHBoxLayout()
+        layout3 = QVBoxLayout()
+
+        layout2.addWidget(Color('red'))
+        layout2.addWidget(Color('yellow'))
+        layout2.addWidget(Color('purple'))
+
+        layout1.addLayout(layout2)
+        # so first all widget of layout2 will come
+
+        layout1.addWidget(Color('green'))
+        # now the widgets of layout1 will appear after layout2
+
+        layout3.addWidget(Color('red'))
+        layout3.addWidget(Color('purple'))
+
+        layout1.addLayout(layout3)
+        # now one more section will be made (last) where widgets of layout3 will appear
+
+        widget = QWidget()
+        widget.setLayout(layout1)
+
+        self.setCentralWidget(widget)
+
+
+class Color(QWidget):
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
