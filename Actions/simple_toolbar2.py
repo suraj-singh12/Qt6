cf49719872a2ae0653b1bb7adf9ec341599c3d2e
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-05 20:45:27 +0530

    added about QAction

--- Actions/simple_toolbar2.py
+++ Actions/simple_toolbar2.py
@@ -6,6 +6,16 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtCore import Qt, QSize
 
+# QAction is a class that provides a way to describe abstract user interfaces.
+# What this means in English, is that you can define multiple interface elements within
+# a single object, unified by the effect that interacting with that element has.
+# For example, it is common to have functions that are represented in the toolbar
+# but also the menu — think of something like Edit->Cut which is present both in the
+# Edit menu but also on the toolbar as a pair of scissors, and also through the
+# keyboard shortcut Ctrl-X (Cmd-X on Mac). Without QAction you would have to define this in multiple places.
+
+# Each QAction has names, status messages, icons and signals that you can connect to (and much more).
+
 
 class MainWindow(QMainWindow):
 
@@ -99,6 +109,9 @@ class MainWindow(QMainWindow):
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
+
+# Note that the keyboard shortcut is associated with the QAction and will
+# still work whether or not the QAction is added to a menu or a toolbar.
 
 # Note that Qt uses your operating system default settings to determine
 # whether to show an icon, text or an icon and text in the toolbar.
