3872196f2dcde5abe7554d7b2e3081e1ff32af14
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-05 20:56:13 +0530

    what are dialogs

--- DialogsAndAlerts/app.py
+++ DialogsAndAlerts/app.py
@@ -1,6 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
+# "Dialogs"
+# Dialogs are useful GUI components that allow you to communicate with the user
+# (hence the name dialog). They are commonly used for file Open/Save, settings,
+# preferences, or for functions that do not fit into the main UI of the
+# application. They are small modal (or blocking) windows that sit in front of
+# the main application until they are dismissed. Qt provides a number of
+# 'special' built-in dialogs for the most common use-cases,
+# allowing you to provide a platform-native user experience.
 
 class MainWindow(QMainWindow):
 
