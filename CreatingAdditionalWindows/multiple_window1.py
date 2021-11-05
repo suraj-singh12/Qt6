df84e4b13f90b20f58aef761afa83f0b87d420ba
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-06 00:37:37 +0530

    creating multiple windows1: (but window just blinks)

---
+++ CreatingAdditionalWindows/multiple_window1.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from random import randint
+
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super(AnotherWindow, self).__init__()
+
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window %d" % randint(0,100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Push for window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        w = AnotherWindow()
+        w.show()
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
