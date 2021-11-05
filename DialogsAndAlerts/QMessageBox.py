e7d9aa5151ad1c070bcf354ba59059eca4969549
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-05 23:24:56 +0530

    best approach for message/info boxes

--- DialogsAndAlerts/QMessageBox.py
+++ DialogsAndAlerts/QMessageBox.py
@@ -17,16 +17,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(button)
 
     def button_clicked(self, s):
-        dlg = QMessageBox(self)
-        dlg.setWindowTitle("Question")
-        dlg.setText("Are you sure?")
-        # all the buttons present in QDialogBox are present in QMessageBox
-        dlg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        dlg.setIcon(QMessageBox.Question)
-        button = dlg.exec()
+        button = QMessageBox.question(self, "Question Dialog", "Are you sure?")
 
-        if button == QMessageBox.Ok:
-            print("Ok")
+        if button == QMessageBox.Yes:
+            print("Yes!")
+        else:
+            print("No!")
 
 # Icon state	                Description
 # QMessageBox.NoIcon	        The message box does not have an icon.
@@ -34,6 +30,18 @@ class MainWindow(QMainWindow):
 # QMessageBox.Information	    The message is informational only.
 # QMessageBox.Warning	        The message is warning.
 # QMessageBox.Critical	        The message indicates a critical problem.
+
+# To make things more simpler, QMessageBox dialogs have a number of methods
+# which can be used to construct these types of message dialog
+
+# QMessageBox.about(parent, title, message)
+# QMessageBox.critical(parent, title, message)
+# QMessageBox.information(parent, title, message)
+# QMessageBox.question(parent, title, message)
+# QMessageBox.warning(parent, title, message)
+
+# The parent parameter is the window which the dialog will be a child of.
+# If you're launching your dialog from your main window, you can just pass in self
 
 
 app = QApplication(sys.argv)
