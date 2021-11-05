7dda20131e1db8f01b3ccbf3d1a42864ebb43341
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-05 15:35:10 +0530

    base file for actions

---
+++ Actions/app.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QToolBar, QAction, QStatusBar
+)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My Awesome App")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
