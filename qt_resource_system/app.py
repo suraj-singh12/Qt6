8eeceb6b15de95aa78fa9fea5cdfe1a9e8ca0552
suraj-singh12 <surajsingh5092@gmail.com>
2021-11-14 10:45:22 +0530

    updated

--- qt_resource_system/app.py
+++ qt_resource_system/app.py
@@ -1,5 +1,9 @@
 import sys
 from PyQt5 import QtGui, QtWidgets
+# compile the .qrc file using below command, and then
+# import the compiled python module generated as a result
+# pyrcc5 resources.qrc -o resources.py
+import resources
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -10,7 +14,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("resource testing")
         self.button = QtWidgets.QPushButton("My button")
 
-        icon = QtGui.QIcon("animal-penguin.png")
+        # resource name (:/virtual-folder-name/alias-name)
+        icon = QtGui.QIcon(":/icons/penguin.png")
         self.button.setIcon(icon)
         self.button.clicked.connect(self.change_icon)
 
@@ -19,7 +24,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.show()
 
     def change_icon(self):
-        icon = QtGui.QIcon("animal-monkey.png")
+        icon = QtGui.QIcon(":/icons/monkey.png")
         self.button.setIcon(icon)
 
 
